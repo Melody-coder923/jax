fb3264f28dde26917b49e3f0c7fcd4719840c4e9
Dimitar (Mitko) Asenov <dasenov@google.com>
2025-03-19 02:17:03 -0700

    [Mosaic GPU] Add layout inference and lowering for `vector.MultiDimReduce`
    PiperOrigin-RevId: 738309891

--- jax/experimental/mosaic/gpu/layout_inference.py
+++ jax/experimental/mosaic/gpu/layout_inference.py
@@ -444,6 +444,94 @@ def _infer_reduction_op_layout(op: vector.ReductionOp) -> OptionalLayouts:
   return None
 
 
+@partial(_add_layout_inference_rule, vector.MultiDimReductionOp)
+def _infer_multi_dim_reduction_op_layout(
+    op: vector.MultiDimReductionOp,
+) -> OptionalLayouts:
+  if inference_utils.has_any_layout_set(op):
+    op_in_layouts = list(inference_utils.in_layouts(op))
+    op_out_layouts = list(inference_utils.out_layouts(op))
+    return op_in_layouts, op_out_layouts
+
+  in_ty = ir.VectorType(op.source.type)
+  out_ty = ir.VectorType(op.result.type)
+  if len(in_ty.shape) != 2 or len(out_ty.shape) != 1:
+    raise NotImplementedError(
+        f"Only 2D -> 1D reductions are supported: {op}"
+    )
+
+  wgmma_layout = layouts_lib.to_layout_attr(fa.WGMMA_LAYOUT)
+  wgmma_row_layout = layouts_lib.to_layout_attr(fa.WGMMA_ROW_LAYOUT)
+  wgmma_col_layout = layouts_lib.to_layout_attr(fa.WGMMA_COL_LAYOUT)
+  reduction_dims = list(op.reduction_dims)
+
+  # Find out the layout of the source.
+  in_layout = inference_utils.value_layout(op.source)
+  if in_layout:
+    if in_layout != wgmma_layout:
+      raise NotImplementedError(f"Unsupported input layout: {in_layout}")
+    if reduction_dims == [0]:
+      out_layout = wgmma_col_layout
+    elif reduction_dims == [1]:
+      out_layout = wgmma_row_layout
+    else:
+      raise NotImplementedError(
+          "Only a single reducation dimension is supported, but got:"
+          f" {reduction_dims}"
+      )
+    return [in_layout, out_layout], [out_layout]
+
+  # The source does not have a layout set. Find out the layout of the result
+  # if possible. We can do this either from consumers or from the acc input.
+  # They should have the same layout.
+
+  # Get user layouts.
+  user_layouts = set()
+  for use in cast(ir.OpResult, op.result).uses:
+    consumer = use.owner
+    operand = consumer.operands[use.operand_number]
+    layout = inference_utils.in_layout_for_operand(consumer, operand)
+    if layout:
+      user_layouts.add(layout)
+
+  user_layout = None
+  if user_layouts:
+    user_layout = _choose_representative_layout(user_layouts)
+    if user_layout is None:
+      raise ValueError(f"Could not choose a best layout from {user_layouts}")
+
+  # Get acc layout.
+  acc_layout = inference_utils.value_layout(op.acc)
+
+  if user_layout is None and acc_layout is None:
+    return None
+  elif user_layout is None:
+    out_layout = acc_layout
+  elif acc_layout is None:
+    out_layout = user_layout
+  else:
+    if acc_layout != user_layout:
+      raise ValueError(
+          "Acc layout does not match user layout: {acc_layout} vs {user_layout}"
+      )
+    out_layout = user_layout
+
+  if out_layout != wgmma_row_layout and out_layout != wgmma_col_layout:
+    raise NotImplementedError(f"Unsupported out layout: {out_layout}")
+
+  if (out_layout == wgmma_row_layout and reduction_dims == [1]) or (
+      out_layout == wgmma_col_layout and reduction_dims == [0]
+  ):
+    in_layout = wgmma_layout
+  else:
+    raise NotImplementedError(
+        f"Unsupported output layout: {out_layout} for reduction dimensions"
+        f" {reduction_dims}"
+    )
+
+  return [in_layout, out_layout], [out_layout]
+
+
 @partial(_add_layout_inference_rule, mgpu.LayoutCastOp)
 def _infer_layout_cast_op_layout(
     layout_cast_op: mgpu.LayoutCastOp,
