98a609fb47791c0a2d35e088e11b81f0910467a4
Justin Fu <justinfu@google.com>
2025-04-10 15:53:48 -0700

    [Pallas][Mosaic GPU] Add support for blackwell mma instruction.
    PiperOrigin-RevId: 746205805

--- jax/_src/pallas/mosaic_gpu/primitives.py
+++ jax/_src/pallas/mosaic_gpu/primitives.py
@@ -33,6 +33,7 @@ from jax._src.lib.mlir import ir
 from jax._src.lib.mlir.dialects import arith as arith_dialect
 from jax._src.lib.mlir.dialects import llvm as llvm_dialect
 from jax._src.lib.mlir.dialects import memref as memref_dialect
+from jax._src.lib.mlir.dialects import gpu as gpu_dialect
 from jax._src.lib.mlir.dialects import nvvm as nvvm_dialect
 from jax._src.pallas import core as pallas_core
 from jax._src.pallas.mosaic_gpu import core as gpu_core
@@ -43,6 +44,7 @@ from jax._src.state import indexing
 from jax._src.state import primitives as state_primitives
 from jax.experimental.mosaic import gpu as mgpu
 from jax.experimental.mosaic.gpu import utils as mgpu_utils
+from jax.experimental.mosaic.gpu import tcgen05
 import jax.numpy as jnp
 
 
@@ -617,6 +619,8 @@ barrier_arrive_p.multiple_results = True
 def _barrier_arrive_abstract_eval(barrier, *args, **params):
   del args, params  # Unused.
   _check_ref(barrier, "barrier", gpu_core.SMEM)
+  if barrier.inner_aval.dtype.for_tensor_core:
+    raise ValueError("Cannot arrive on a tensor core barrier.")
   return (), {gpu_core._memory_effect}
 
 
@@ -704,13 +708,14 @@ def _barrier_wait_lowering(
     barrier,
     *flat_transforms,
     transforms_treedef,
+    for_tensor_core,
 ):
   del ctx  # Unused.
   transforms = transforms_treedef.unflatten(flat_transforms)
   indexer = _extract_barrier_indexer(transforms)
   if indexer is not None:
     barrier = barrier.__getitem__(*map(lowering._as_index, indexer.indices))
-  barrier.wait()
+  barrier.wait(for_tensor_core=for_tensor_core)
   return ()
 
 
@@ -719,9 +724,11 @@ def barrier_wait(barrier: pallas_core.AbstractMemoryRef) -> None:
   barrier, transforms = state_primitives.get_ref_and_transforms(
       barrier, None, "barrier_wait", force_trailing_indexer=False,
   )
+  for_tensor_core = barrier.inner_aval.dtype.for_tensor_core
   flat_transforms, transforms_treedef = tree_util.tree_flatten(transforms)
   barrier_wait_p.bind(
-      barrier, *flat_transforms, transforms_treedef=transforms_treedef
+      barrier, *flat_transforms, transforms_treedef=transforms_treedef,
+      for_tensor_core=for_tensor_core,
   )
 
 
@@ -1110,6 +1117,159 @@ def _wgmma_accumulator_deref_lowering(ctx: lowering.LoweringRuleContext, acc):
       else acc
   )
 
+
+# MMA for TensorCore gen 5.
+tcgen05_mma_p = jax_core.Primitive("tcgen05_mma")
+tcgen05_mma_p.multiple_results = True
+
+def tcgen05_mma(acc: gpu_core.AbstractMemoryRef,
+                a: gpu_core.AbstractMemoryRef,
+                b: gpu_core.AbstractMemoryRef,
+                barrier: gpu_core.AbstractMemoryRef,
+                accumulate: bool = False,
+                collective: bool = False):
+  """Asynchronous matrix-multiply accumulate for TensorCore gen 5 (Blackwell).
+
+  Args:
+    acc: The accumulator. Must be a TMEM Ref.
+    a: The left-hand side. Must be a TMEM/SMEM Ref.
+    b: The right-hand side. Must be an SMEM Ref.
+    barrier: Barrier Ref for synchronizing with the tensor core. Should have
+      for_tensor_core set to True.
+    accumulate: Whether to accumulate into acc or overwrite it.
+    collective: Whether the MMA is being executed on paired blocks. The MMA will
+      only be issued by one block between the pair.
+  """
+  if isinstance(a, pallas_core.TransformedRef):
+    a_transforms_leaves, a_transforms_tree = jax.tree.flatten(a.transforms)
+    a = a.ref
+  else:
+    a_transforms_leaves, a_transforms_tree = [], None
+
+  if isinstance(b, pallas_core.TransformedRef):
+    b_transforms_leaves, b_transforms_tree = jax.tree.flatten(b.transforms)
+    b = b.ref
+  else:
+    b_transforms_leaves, b_transforms_tree = [], None
+
+  tcgen05_mma_p.bind(acc, a, b, barrier,
+                      *a_transforms_leaves, *b_transforms_leaves,
+                      a_transforms_tree=a_transforms_tree,
+                      b_transforms_tree=b_transforms_tree,
+                      accumulate=accumulate,
+                      collective=collective)
+
+@tcgen05_mma_p.def_abstract_eval
+def _tcgen05_mma_abstract_eval(acc, a, b, barrier,
+                               *transforms_leaves,
+                               a_transforms_tree, b_transforms_tree,
+                               accumulate,
+                               collective):
+  del transforms_leaves, a_transforms_tree, b_transforms_tree, collective
+  if acc.memory_space != gpu_core.GPUMemorySpace.TMEM:
+    raise ValueError("Accumulator must be a TMEM Ref.")
+  if a.memory_space not in (gpu_core.GPUMemorySpace.TMEM,
+                            gpu_core.GPUMemorySpace.SMEM):
+    raise ValueError("LHS must be a TMEM or SMEM Ref.")
+  if b.memory_space != gpu_core.GPUMemorySpace.SMEM:
+    raise ValueError("RHS must be a SMEM Ref.")
+  if not barrier.inner_aval.dtype.for_tensor_core:
+    raise ValueError("MMA barrier must have for_tensor_core set to True.")
+  return []
+
+@lowering.register_lowering_rule(
+    tcgen05_mma_p, *gpu_core.LANExWARP_SEMANTICS)
+def _tcgen05_mma_lowering(
+    ctx: lowering.LoweringRuleContext,
+    acc: tcgen05.TMEMRef,
+    a_ref,
+    b_ref,
+    barrier_ref: mgpu.BarrierRef,
+    *transforms_leaves,
+    a_transforms_tree,
+    b_transforms_tree,
+    accumulate: bool,
+    collective: bool,
+):
+  _, a_aval, *_ = ctx.avals_in
+  lhs_swizzle: int | None = None
+  lhs_transpose: bool = False
+  if a_transforms_tree is not None:
+    a_transforms_leaves, b_transforms_leaves = util.split_list(
+        transforms_leaves, [a_transforms_tree.num_leaves]
+    )
+
+    a_transforms = a_transforms_tree.unflatten(a_transforms_leaves)
+    a_ref, a_transforms = lowering._handle_transforms(
+        a_ref, a_transforms, handle_transposes=True, handle_reshapes=False
+    )
+    match a_transforms:
+      case (gpu_core.UnswizzleRef(lhs_swizzle), gpu_core.UntileRef(lhs_tiling)):
+        lhs_transpose = False
+      case (
+          gpu_core.UnswizzleRef(lhs_swizzle),
+          gpu_core.UntileRef(lhs_tiling),
+          gpu_core.TransposeRef((1, 0)),
+      ):
+        lhs_transpose = True
+      case _:
+        raise ValueError(
+            f"Unsupported transforms: {a_transforms}."
+        )
+    swizzle_elems = lhs_swizzle // a_aval.dtype.itemsize
+    if lhs_tiling != (8, swizzle_elems):
+      raise NotImplementedError("MMA lhs tiling does not fit swizzle")
+  else:
+    b_transforms_leaves = transforms_leaves  # type: ignore
+
+  b_transforms = b_transforms_tree.unflatten(b_transforms_leaves)
+  b_ref, b_transforms = lowering._handle_transforms(
+      b_ref, b_transforms, handle_transposes=True, handle_reshapes=False
+  )
+  match b_transforms:
+    case (gpu_core.UnswizzleRef(rhs_swizzle), gpu_core.UntileRef(rhs_tiling)):
+      rhs_transpose = False
+    case (
+        gpu_core.UnswizzleRef(rhs_swizzle),
+        gpu_core.UntileRef(rhs_tiling),
+        gpu_core.TransposeRef((1, 0)),
+    ):
+      rhs_transpose = True
+    case _:
+      raise ValueError(
+          f"Unsupported transforms: {b_transforms}."
+      )
+
+  if lhs_swizzle is not None:
+    swizzle_elems = rhs_swizzle // a_aval.dtype.itemsize
+    if rhs_swizzle != lhs_swizzle:
+      raise NotImplementedError("MMA rhs swizzle must match lhs swizzle")
+    if rhs_tiling != (8, swizzle_elems):
+      raise NotImplementedError("MMA rhs tiling does not fit swizzle")
+  if lhs_transpose or rhs_transpose:
+    raise NotImplementedError("MMA does not support transpose")
+
+  predicate = ctx.module_ctx.single_lane_predicate
+  if collective:
+    index = ir.IndexType.get()
+    is_leader_block = arith_dialect.cmpi(
+        arith_dialect.CmpIPredicate.eq,
+        ctx.launch_ctx.cluster_idx(gpu_dialect.Dimension.x), mgpu.c(0, index))
+    predicate = arith_dialect.andi(predicate, is_leader_block)
+  with mgpu.when(predicate):
+    tcgen05.mma(
+              acc,
+              a_ref,
+              b_ref,
+              a_swizzle=rhs_swizzle,
+              b_swizzle=lhs_swizzle,
+              accumulate=accumulate,
+              collective=collective,
+          )
+    tcgen05.commit_arrive(barrier_ref,
+                          collective=collective,
+                          ctx=ctx.launch_ctx)
+  return []
 
 class Layout(enum.Enum):
   #: [m, n] matrix, where m % 64 == 0 == n % 8.
--- tests/pallas/mosaic_gpu_test.py
+++ tests/pallas/mosaic_gpu_test.py
@@ -2075,6 +2075,59 @@ class PallasCallSm100ATest(PallasSm100ATest):
     # Test that this runs without errors.
     jax.block_until_ready(kernel())
 
+  @parameterized.parameters(
+      ((128, 128),),
+  )
+  def test_simple_matmul(self, shape):
+    # Test a matmul with a single block.
+    dtype = jnp.float16
+    swizzle = 128
+    elems_128b = swizzle // jnp.dtype(dtype).itemsize
+    transforms = (
+        plgpu.TilingTransform((8, elems_128b)),
+        plgpu.SwizzleTransform(128),
+    )
+
+    warp_mesh = plgpu.WarpMesh(axis_name="w")
+    def kernel(a_smem, b_smem, out_ref, acc_tmem, scratch_smem, barrier_ref):
+      @pl.core_map(warp_mesh)
+      def _():
+        warp_idx = lax.axis_index("w")
+        @pl.when(warp_idx == 0)
+        def _():
+          plgpu.tcgen05_mma(acc_tmem,
+                            a_smem,
+                            b_smem,
+                            barrier_ref,
+                            accumulate=False)
+      plgpu.barrier_wait(barrier_ref)
+      scratch_smem[...] = acc_tmem[...].astype(jnp.float16)
+      plgpu.commit_smem()
+      plgpu.copy_smem_to_gmem(scratch_smem, out_ref)
+      plgpu.wait_smem_to_gmem(0)
+
+    f = self.pallas_call(
+        kernel,
+        in_specs=(
+            plgpu.GPUBlockSpec(transforms=transforms,
+                               memory_space=plgpu.SMEM),
+            plgpu.GPUBlockSpec(transforms=transforms,
+                               memory_space=plgpu.SMEM),
+        ),
+        out_specs=plgpu.GPUBlockSpec(memory_space=plgpu.GMEM),
+        out_shape=jax.ShapeDtypeStruct(shape, jnp.float16),
+        scratch_shapes=[
+            plgpu.TMEM((128, 128), jnp.float32),
+            plgpu.SMEM(shape, jnp.float16, transforms=transforms),
+            plgpu.Barrier(num_arrivals=1, for_tensor_core=True),
+        ],
+    )
+    x = jax.random.uniform(jax.random.key(0), shape=shape, dtype=jnp.float16)
+    y = jax.random.uniform(jax.random.key(1), shape=shape, dtype=jnp.float16)
+    result = f(x, y)
+    expected = x @ y
+    np.testing.assert_allclose(result, expected, rtol=1e-3)
+
 
 class PallasCallSm100AWGTest(
     PallasCallSm100ATest, lowering_semantics=plgpu.LoweringSemantics.Warpgroup
